9ed50a51031c23408c6c0e0ce90ff41c5ad46881
Eduardo Serrano <eduardomserrano@outlook.com>
2022-04-13 00:50:51 +0100

    test custom action 9

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -3,4 +3,5 @@ Write-Host 'Starting dotnet cli tool'
 $templateFilepath = "/github/workspace/$templateFilepath"
 Write-Host "templateFilepath $templateFilepath"
 Write-Host "issueFormBody $issueFormBody"
+cat $templateFilepath
 dotnet '/app/GitHubIssuesParserCli.dll' parse-issue-form -t $templateFilepath -i $issueFormBody
