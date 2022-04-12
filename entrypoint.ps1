20b08ca5e83e4daf9551c2243414ca6acb6e5318
Eduardo Serrano <eduardomserrano@outlook.com>
2022-04-13 00:56:24 +0100

    test custom action 10

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,6 +1,5 @@
 param ($templateFilepath, $issueFormBody)
 Write-Host 'Starting dotnet cli tool'
-$templateFilepath = "/github/workspace/$templateFilepath"
 Write-Host "templateFilepath $templateFilepath"
 Write-Host "issueFormBody $issueFormBody"
 cat $templateFilepath
