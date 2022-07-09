826fe50b7a95e62537e8bf3a3cf7d41743bb7588
Eduardo Serrano <eduardomserrano@outlook.com>
2022-07-09 13:20:22 +0100

    test using $args on entrypoint for docker 17

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -5,33 +5,49 @@
 #   $issueFormBodyOption,
 #   $issueFormBody)
 
+function Escape-SingleQuotes
+{
+  param ($value)
+
+  return $value -replace '''', ''''''
+}
+
 Write-Output "::group::Run dotnet GitHub issue form parser"
 
 
-Write-Output "==========================="
-Write-Output $commandName
-Write-Output $templateFilepathOption
-Write-Output $templateFilepath
-Write-Output $issueFormBodyOption
-Write-Output $issueFormBody
-Write-Output "==========================="
-Write-Output $args.Count
-Write-Output $args
-Write-Output "==========================="
+# Write-Output "==========================="
+# Write-Output $commandName
+# Write-Output $templateFilepathOption
+# Write-Output $templateFilepath
+# Write-Output $issueFormBodyOption
+# Write-Output $issueFormBody
+# Write-Output "==========================="
+# Write-Output $args.Count
+# Write-Output $args
+# Write-Output "==========================="
 
-$templateFilepath = $args[2]
-$templateFilepath = "'$templateFilepath'"
-$issueFormBody =  $args[4]
-# $issueFormBody =  $issueFormBody -replace '''', '`'''
-$issueFormBody =  $issueFormBody -replace '''', ''''''
-$issueFormBody = "'$issueFormBody'"
-$inputArgs =-join($args[0], " ", $args[1], " ", $templateFilepath, " ", $args[3], " ", $issueFormBody)
-$command = "dotnet '/app/GitHubIssuesParserCli.dll' $inputArgs"
+# $templateFilepath = $args[2]
+# $templateFilepath = "'$templateFilepath'"
+# $issueFormBody = $args[4]
+# # $issueFormBody =  $issueFormBody -replace '''', '`'''
+# $issueFormBody = $issueFormBody -replace '''', ''''''
+# $issueFormBody = "'$issueFormBody'"
+# $inputArgs = -join ($args[0], " ", $args[1], " ", $templateFilepath, " ", $args[3], " ", $issueFormBody)
+# $command = "dotnet '/app/GitHubIssuesParserCli.dll' $inputArgs"
+
+
+$command = ""
+foreach ($arg in $args)
+{
+  $command += Escape-SingleQuotes $arg
+}
+
 Write-Output $command
 $output = Invoke-Expression $command
-if($LASTEXITCODE -ne 0 ) {
-    Write-Output "::error::GitHub issue form parser didn't complete successfully. See the step's log for more details."
-    exit $LASTEXITCODE
+if ($LASTEXITCODE -ne 0 ) 
+{
+  Write-Output "::error::GitHub issue form parser didn't complete successfully. See the step's log for more details."
+  exit $LASTEXITCODE
 }
 Write-Output "::set-output name=parsed-issue::$output"
 Write-Output "::endgroup::"
