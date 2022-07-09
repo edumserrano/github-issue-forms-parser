ee1f03dcce1ec81b320fa587a58e7a92b76e3f52
Eduardo Serrano <eduardomserrano@outlook.com>
2022-07-09 13:55:04 +0100

    test using $args on entrypoint for docker 22

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,66 +1,63 @@
-# param (
-#   $commandName,
-#   $templateFilepathOption,
-#   $templateFilepath,
-#   $issueFormBodyOption,
-#   $issueFormBody)
+Main # invoke entrypoint function
+
+function Main()
+{
+  Write-Output "::group::Run dotnet GitHub issue form parser"
+  $command = Sanitize-InputArgs $args 
+  Write-Output $command
+  $output = Invoke-Expression $command
+  if ($LASTEXITCODE -ne 0 ) 
+  {
+    Write-Output "::error::GitHub issue form parser didn't complete successfully. See the step's log for more details."
+    exit $LASTEXITCODE
+  }
+  Write-Output "::set-output name=parsed-issue::$output"
+  Write-Output "::endgroup::"
+  
+  Write-Output "::group::dotnet GitHub issue form parser output"
+  Write-Output $output
+  Write-Output "::endgroup::"
+  
+  Write-Output "::group::dotnet GitHub issue form parser output indented"
+  $outputAsJson = ConvertFrom-Json $output
+  COnvertTo-Json $outputAsJson
+  Write-Output "::endgroup::"
+}
+
+function Sanitize-InputArgs
+{
+  [OutputType([string])]
+  param ([string[]] $inputArgs)
+
+  $sanitizedArgs = [string[]]::new($inputArgs.Count)
+  for ($i = 0; $i -lt $inputArgs.Count; $i++)
+  {
+    $arg = $inputArgs[$i]
+    $arg = Escape-SingleQuotes $arg
+    if (!$arg.StartsWith("'"))
+    {
+      $arg  = Add-SingleQuote $arg
+    }
+
+    $sanitizedArgs[$i] = $arg
+  }
+
+  return $($sanitizedArgs -join " ")
+}
+
 
 function Escape-SingleQuotes
 {
-  param ($value)
+  [OutputType([string])]
+  param ([string] $value)
 
   return $value -replace '''', ''''''
 }
 
-Write-Output "::group::Run dotnet GitHub issue form parser"
-
-
-# Write-Output "==========================="
-# Write-Output $commandName
-# Write-Output $templateFilepathOption
-# Write-Output $templateFilepath
-# Write-Output $issueFormBodyOption
-# Write-Output $issueFormBody
-# Write-Output "==========================="
-# Write-Output $args.Count
-# Write-Output $args
-# Write-Output "==========================="
-
-# $templateFilepath = $args[2]
-# $templateFilepath = "'$templateFilepath'"
-# $issueFormBody = $args[4]
-# # $issueFormBody =  $issueFormBody -replace '''', '`'''
-# $issueFormBody = $issueFormBody -replace '''', ''''''
-# $issueFormBody = "'$issueFormBody'"
-# $inputArgs = -join ($args[0], " ", $args[1], " ", $templateFilepath, " ", $args[3], " ", $issueFormBody)
-# $command = "dotnet '/app/GitHubIssuesParserCli.dll' $inputArgs"
-
-
-for ($i = 0; $i -lt $args.Count; $i++)
+function Add-SingleQuote
 {
-  $args[$i] = Escape-SingleQuotes $args[$i]
-  if (!$args[$i].StartsWith("'"))
-  {
-    $args[$i] = "'$args[$i]'"
-  }
+  [OutputType([string])]
+  param ([string] $value)
+
+  return "'$value'"
 }
-
-$command = $($args -join " ")
-Write-Output $command
-$output = Invoke-Expression $command
-if ($LASTEXITCODE -ne 0 ) 
-{
-  Write-Output "::error::GitHub issue form parser didn't complete successfully. See the step's log for more details."
-  exit $LASTEXITCODE
-}
-Write-Output "::set-output name=parsed-issue::$output"
-Write-Output "::endgroup::"
-
-Write-Output "::group::dotnet GitHub issue form parser output"
-Write-Output $output
-Write-Output "::endgroup::"
-
-Write-Output "::group::dotnet GitHub issue form parser output indented"
-$outputAsJson = ConvertFrom-Json $output
-COnvertTo-Json $outputAsJson
-Write-Output "::endgroup::"
