be709916cae2343e1fc825e04d83d888385284e6
Eduardo Serrano <eduardomserrano@outlook.com>
2022-07-09 11:25:37 +0100

    test using $args on entrypoint for docker

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,17 +1,5 @@
-param ($templateFilepath, $issueFormBody)
-
-Write-Output "::group::GitHub issue form template"
-Write-Output "Template filepath: '$templateFilepath'"
-$template = Get-Content $templateFilepath
-Write-Output $template
-Write-Output "::endgroup::"
-
-Write-Output "::group::GitHub issue form body"
-Write-Output $issueFormBody
-Write-Output "::endgroup::"
-
 Write-Output "::group::Run dotnet GitHub issue form parser"
-$output = dotnet '/app/GitHubIssuesParserCli.dll' parse-issue-form -t $templateFilepath -i $issueFormBody
+$output = dotnet '/app/GitHubIssuesParserCli.dll' $args
 if($LASTEXITCODE -ne 0 ) {
     Write-Output "::error::GitHub issue form parser didn't complete successfully. See the step's log for more details."
     exit $LASTEXITCODE
