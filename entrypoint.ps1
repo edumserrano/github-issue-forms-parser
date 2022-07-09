2495c3866bcd9676f4efb7aba09e7b815a594328
Eduardo Serrano <eduardomserrano@outlook.com>
2022-07-09 11:39:00 +0100

    test using $args on entrypoint for docker 3

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,19 +1,17 @@
 Write-Output "::group::Run dotnet GitHub issue form parser"
-$args.Count
+$output = dotnet '/app/GitHubIssuesParserCli.dll' $args[0]
+if($LASTEXITCODE -ne 0 ) {
+    Write-Output "::error::GitHub issue form parser didn't complete successfully. See the step's log for more details."
+    exit $LASTEXITCODE
+}
+Write-Output "::set-output name=parsed-issue::$output"
+Write-Output "::endgroup::"
 
-# $output = dotnet '/app/GitHubIssuesParserCli.dll' $args
-# if($LASTEXITCODE -ne 0 ) {
-#     Write-Output "::error::GitHub issue form parser didn't complete successfully. See the step's log for more details."
-#     exit $LASTEXITCODE
-# }
-# Write-Output "::set-output name=parsed-issue::$output"
-# Write-Output "::endgroup::"
+Write-Output "::group::dotnet GitHub issue form parser output"
+Write-Output $output
+Write-Output "::endgroup::"
 
-# Write-Output "::group::dotnet GitHub issue form parser output"
-# Write-Output $output
-# Write-Output "::endgroup::"
-
-# Write-Output "::group::dotnet GitHub issue form parser output indented"
-# $outputAsJson = ConvertFrom-Json $output
-# COnvertTo-Json $outputAsJson
-# Write-Output "::endgroup::"
+Write-Output "::group::dotnet GitHub issue form parser output indented"
+$outputAsJson = ConvertFrom-Json $output
+COnvertTo-Json $outputAsJson
+Write-Output "::endgroup::"
