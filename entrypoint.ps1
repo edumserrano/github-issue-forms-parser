1194865edb84a159b845a7682daa5fa62b02153d
Eduardo Serrano <eduardomserrano@outlook.com>
2022-07-09 12:13:09 +0100

    test using $args on entrypoint for docker 11

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,17 +1,11 @@
-param ($templateFilepath, $issueFormBody)
-
-Write-Output "::group::GitHub issue form template"
-Write-Output "Template filepath: '$templateFilepath'"
-$template = Get-Content $templateFilepath
-Write-Output $template
-Write-Output "::endgroup::"
-
-Write-Output "::group::GitHub issue form body"
-Write-Output $issueFormBody
-Write-Output "::endgroup::"
-
 Write-Output "::group::Run dotnet GitHub issue form parser"
-$output = dotnet '/app/GitHubIssuesParserCli.dll' parse-issue-form -t $templateFilepath -i $issueFormBody
+$inputArgs = "$($args -join " ")"
+Write-Output "--------$inputArgs--------"
+$command = "dotnet '/app/GitHubIssuesParserCli.dll' $inputArgs"
+Write-Output $command
+$command = $command -replace '''', '''''' 
+Write-Output $command
+$output = Invoke-Expression $command
 if($LASTEXITCODE -ne 0 ) {
     Write-Output "::error::GitHub issue form parser didn't complete successfully. See the step's log for more details."
     exit $LASTEXITCODE
