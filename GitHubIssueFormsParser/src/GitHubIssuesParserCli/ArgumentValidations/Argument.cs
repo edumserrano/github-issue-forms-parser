5ce0748bcfc8705bb3f5d76f8a502ef135df34de
Eduardo Serrano <eduardomserrano@outlook.com>
2023-05-16 17:33:27 +0100

    fix warnings

--- GitHubIssueFormsParser/src/GitHubIssuesParserCli/ArgumentValidations/Argument.cs
+++ GitHubIssueFormsParser/src/GitHubIssuesParserCli/ArgumentValidations/Argument.cs
@@ -2,13 +2,13 @@ namespace GitHubIssuesParserCli.ArgumentValidations;
 
 internal static class Argument
 {
-    public static T NotNull<T>([NotNull] this T? value, [CallerArgumentExpression("value")] string name = "")
+    public static T NotNull<T>([NotNull] this T? value, [CallerArgumentExpression(nameof(value))] string name = "")
         where T : class
     {
         return value ?? throw new ArgumentNullException(name);
     }
 
-    internal static string NotNullOrWhiteSpace([NotNull] this string? value, [CallerArgumentExpression("value")] string name = "")
+    internal static string NotNullOrWhiteSpace([NotNull] this string? value, [CallerArgumentExpression(nameof(value))] string name = "")
     {
         var message = $"{name} cannot be null or whitespace.";
         return string.IsNullOrWhiteSpace(value)
